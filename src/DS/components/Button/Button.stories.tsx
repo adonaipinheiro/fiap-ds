6641ffe3a059ac83396bc5a45b2a1bb5d72d87dc
Adonai Pinheiro <adonaijpinheiro@gmail.com>
2025-01-24 15:09:55 -0300

    att

--- src/DS/components/Button/Button.stories.tsx
+++ src/DS/components/Button/Button.stories.tsx
@@ -2,6 +2,7 @@ import { Meta, StoryFn } from "@storybook/react";
 import { DS_Button } from ".";
 import { FaSignInAlt } from "react-icons/fa";
 import type { DS_ButtonProps } from "./types";
+import { fn } from "@storybook/test";
 
 export default {
   title: "Components/Button",
@@ -12,6 +13,7 @@ export default {
     leftIcon: { control: "object" },
     state: { controle: "text" },
   },
+  args: { onClick: fn() },
 } as Meta;
 
 const Template: StoryFn<DS_ButtonProps> = (args) => <DS_Button {...args} />;
@@ -27,4 +29,10 @@ WithIcon.args = {
   children: "Entrar",
   state: "default",
   rightIcon: <FaSignInAlt />,
+};
+
+export const Outline = Template.bind({});
+Outline.args = {
+  children: "Entrar",
+  state: "outline",
 };
